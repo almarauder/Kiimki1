7aaa76f7bc5b3aa3df4b0a6dd2a776c15a3f0f78
almasrauder <darking.picqaxe@gmail.com>
2024-12-06 07:52:10 +0500

    Initial Commit

---
+++ Kiimki/KiimkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiimkiApp.swift
+//  Kiimki
+//
+//  Created by Darking Almas on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct KiimkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KiimkiTests/KiimkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KiimkiTests.swift
+//  KiimkiTests
+//
+//  Created by Darking Almas on 06.12.2024.
+//
+
+import Testing
+@testable import Kiimki
+
+struct KiimkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
